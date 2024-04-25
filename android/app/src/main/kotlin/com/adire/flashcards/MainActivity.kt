d8c758eb884e737610511668552047aaf7c7572e
Becca-Saka <thebeccasaka@gmail.com>
2024-04-25 22:43:10 +0100

    initial commit

---
+++ android/app/src/main/kotlin/com/adire/flashcards/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.adire.flashcards
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flashcards/firebase_options.dart';
+import 'package:flashcards/ui/home_view.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      home: const HomeView(),
+    );
+  }
+}
