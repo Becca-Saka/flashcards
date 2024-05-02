cd16eae1435b3b90813344a3dcea09c5179f1702
Lord-Chris <maduekechris65@gmail.com>
2024-05-02 02:14:41 +0100

    FIX: Render flex issue

--- lib/ui/collections/quiz_view.dart
+++ lib/ui/collections/quiz_view.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flashcards/model/quiz_model.dart';
 import 'package:flashcards/shared/custom_expansion_tile.dart';
 import 'package:flashcards/shared/shared.dart';
 import 'package:flutter/material.dart';
@@ -71,81 +72,7 @@ class QuizView extends StatelessWidget {
                   // shrinkWrap: true,
                   itemBuilder: (context, index, pageindex) {
                     final file = controller.selectedCollection!.quizzes[index];
-                    return Card(
-                      color: Colors.primaries[Random().nextInt(18)],
-                      child: Container(
-                        color: Colors.primaries[Random().nextInt(18)],
-                        padding: const EdgeInsets.all(20),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Expanded(
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  const Spacer(),
-                                  Text(
-                                    'Question ${index + 1}',
-                                    style: AppTextStyle.medium14.copyWith(
-                                      color: Colors.white,
-                                    ),
-                                  ),
-                                  const AppSpacing(v: 20),
-                                  Text(
-                                    file.question,
-                                    style: AppTextStyle.extraBold24.copyWith(
-                                      color: Colors.white,
-                                      fontSize: 25,
-                                    ),
-                                  ),
-                                  const Spacer(),
-                                ],
-                              ),
-                            ),
-                            Theme(
-                              data: Theme.of(context).copyWith(
-                                dividerColor: Colors.transparent,
-                              ),
-                              child: CustomExpansionTile(
-                                childrenPadding: EdgeInsets.zero,
-                                tilePadding: EdgeInsets.zero,
-                                iconColor: Colors.white,
-                                collapsedIconColor: Colors.white,
-                                title: Text(
-                                  'Show answer',
-                                  style: AppTextStyle.bold16.copyWith(
-                                    color: Colors.white,
-                                    fontSize: 14,
-                                  ),
-                                ),
-                                collapsedTitle: Text(
-                                  'Hide answer',
-                                  style: AppTextStyle.bold16.copyWith(
-                                    color: Colors.white,
-                                    fontSize: 14,
-                                  ),
-                                ),
-                                children: [
-                                  SizedBox(
-                                    height: MediaQuery.of(context).size.height *
-                                        0.2,
-                                    child: SingleChildScrollView(
-                                      child: Text(
-                                        file.answer * 50,
-                                        style: AppTextStyle.bold16.copyWith(
-                                          color: Colors.white,
-                                          fontSize: 14,
-                                        ),
-                                      ),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            )
-                          ],
-                        ),
-                      ),
-                    );
+                    return QuestionCard(file: file, index: index);
                   },
                 ),
                 const AppSpacing(v: 40),
@@ -200,6 +127,99 @@ class QuizView extends StatelessWidget {
           ),
         );
       },
+    );
+  }
+}
+
+class QuestionCard extends StatelessWidget {
+  const QuestionCard({
+    super.key,
+    required this.file,
+    required this.index,
+  });
+
+  final QuizModel file;
+  final int index;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: Colors.primaries[Random().nextInt(18)],
+      child: Container(
+        color: Colors.primaries[Random().nextInt(18)],
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Question ${index + 1}',
+                    style: AppTextStyle.medium14.copyWith(
+                      color: Colors.white,
+                    ),
+                  ),
+                  const AppSpacing(v: 20),
+                  Flexible(
+                    child: Text(
+                      file.question,
+                      style: AppTextStyle.extraBold24.copyWith(
+                        color: Colors.white,
+                        fontSize: 25,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Theme(
+              data: Theme.of(context).copyWith(
+                dividerColor: Colors.transparent,
+              ),
+              child: CustomExpansionTile(
+                childrenPadding: EdgeInsets.zero,
+                tilePadding: EdgeInsets.zero,
+                iconColor: Colors.white,
+                collapsedIconColor: Colors.white,
+                title: Text(
+                  'Show answer',
+                  style: AppTextStyle.bold16.copyWith(
+                    color: Colors.white,
+                    fontSize: 14,
+                  ),
+                ),
+                collapsedTitle: Text(
+                  'Hide answer',
+                  style: AppTextStyle.bold16.copyWith(
+                    color: Colors.white,
+                    fontSize: 14,
+                  ),
+                ),
+                children: [
+                  ConstrainedBox(
+                    constraints: BoxConstraints(
+                      maxHeight: MediaQuery.of(context).size.height * 0.2,
+                    ),
+                    child: SingleChildScrollView(
+                      child: Text(
+                        file.answer,
+                        style: AppTextStyle.bold16.copyWith(
+                          color: Colors.white,
+                          fontSize: 14,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
